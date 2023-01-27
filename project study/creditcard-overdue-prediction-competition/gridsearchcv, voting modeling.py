dc77a8d5fb9f8bd2df52374a00ea9e844faef2d3
Jiyeong Son <122194456+zi0-hand@users.noreply.github.com>
2023-01-28 00:16:03 +0900

    gridsearchcv, voting 함수 생성

--- project study/creditcard-overdue-prediction-competition/gridsearchcv, voting modeling.py
+++ project study/creditcard-overdue-prediction-competition/gridsearchcv, voting modeling.py
@@ -1,24 +1,11 @@
 from sklearn.ensemble import VotingClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 
-param={
-    'num_leaves':[32,64],
-    'min_data_in_leaf':[1,5,10],
-    'colsample_bytree':[0.8,1],
-    'n_estimators':[50, 100, 150]
-}
-
 def gridcv_process(model, param, cv_num, n_thread):
     model_cv = GridSearchCV(model, param, cv=cv_num, n_jobs=n_thread, refit=True)
     model_cv.fit(X_train, y_train)
-    print('GridSearchCV 최적 파라미터:', GS_LGB.best_params_)
-    print('GridSearchCV 최고 정확도:{0:.4f}'.format(GS_LGB.best_score_)
-
-rf_clf = RandomForestClassifier(random_state=42)
-gb_clf = GradientBoostingClassifier(random_state=42)
-xgb_clf = XGBClassifier(random_state=42)
-
-models_clf = [rf_clf, gb_clf, xgb_clf]
+    print('GridSearchCV 최적 파라미터:', model_cv.best_params_)
+    print('GridSearchCV 최고 정확도:{0:.4f}'.format(model_cv.best_score_))
 
 def voting_process(models):
     lst = []
